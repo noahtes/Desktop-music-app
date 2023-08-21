5a95c1c76c6272c942f2640742431079c88f7d51
NohTes <noahtse3@gmail.com>
2023-08-21 15:58:09 -0600

    adding DOM

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let currentMusic = 0;
 const music = document.querySelector('#audio');
 const seekBar = document.querySelector('.seek-bar');
 const songName = document.querySelector('music-name');
+const artist = document.querySelector('artist-name');
 
 
 
